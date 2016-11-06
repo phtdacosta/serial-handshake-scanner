0e135011c460e73a8d6ecfe23316d5b032bef708
Pedro Costa <pedrohenriqueteodorodacosta@gmail.com>
2016-11-06 21:57:06 -0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 # 11/05/2016
 #
 # Handshake Serial Port Monitor/Scanner
-# After a successful connection it works as a one-way - computer to device - terminal
+# After a successful connection it works as a one-way - computer to device or device to computer - terminal too
 #
-# v1.0 alpha
+# v1.1 alpha
 #
 
 import sys
@@ -14,8 +14,8 @@ import serial
 from array import array
 
 def serial_ports():
-# lists serial port names for both win32 and linux operating systems
-#
+	# lists serial port names for both win32 and linux operating systems
+	#
     if sys.platform.startswith('win'):
         ports = ['COM%s' % (i + 1) for i in range(256)]
     elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
@@ -41,14 +41,22 @@ def serial_ports():
 def run():
 	connected = False
 	port_id = ""
+	interaction_mode = ""
+	baud_rate = ""
+	handshake_char = ""
+	# set interaction mode with the serial port
+	#
+	while interaction_mode is not "0" and interaction_mode is not "1":
+		interaction_mode = raw_input("\n0 - RECEIVE-ONLY\n1 - TRANSMIT-ONLY\n> ")
 	# set device baud rate here
 	#
-	baud_rate = raw_input("BAUD RATE: ")
+	while not baud_rate.isdigit():
+		baud_rate = raw_input("\nBAUD RATE\n> ")
 	# set the handshake char here
 	#
-	handshake_char = raw_input("HANDSHAKE CHARACTER: ")
+	handshake_char = raw_input("\nHANDSHAKE CHARACTER\n> ")
 
-	print("Scanning . . ")
+	print("\nScanning . . ")
 	# infinite loop runs the program forever
 	#
 	while True:
@@ -64,20 +72,28 @@ def run():
 			for port in serial_ports():
 				if not connected:
 					try:
-						ser = serial.Serial(port, (int)(baud_rate))
+						ser = serial.Serial(port, int(baud_rate))
 						char = ser.read()
 						if (char == handshake_char):
-							print("*** CONNECTED ***\n(at "+port+" port)\n\nTYPE INPUT")
+							print("*** CONNECTED ***\n(at "+port+" port)\n")
+							if interaction_mode == "0":
+								print("\nINPUT RECEIVED\n")
+							elif interaction_mode == "1":
+								print("\nTYPE INPUT\n")
 							connected = True
 							port_id = port
 					except:
 						pass
 		try:
 			while connected:
-				# here we can set any kind of input to be streamed to the serial port
-				#
-				s = raw_input("> ")
-				ser.write(array("B",s))
+				if interaction_mode == "0":
+					s = ser.read()
+					sys.stdout.write(s)
+				elif interaction_mode == "1":
+					# here we can set any kind of input to be streamed to the serial port
+					#
+					s = raw_input()
+					ser.write(array("B",s))
 		except:
 			connected = False
 			port_id = ""
